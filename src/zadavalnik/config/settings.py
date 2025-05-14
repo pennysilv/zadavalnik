dbe09ac837c3b5d4f5de109fe134f6c1579d3b1f
Anton Lobachev <anvlobachev@gmail.com>
2025-05-14 11:25:04 +0500

    Test botgub api

--- src/zadavalnik/config/settings.py
+++ src/zadavalnik/config/settings.py
@@ -5,5 +5,7 @@ load_dotenv(".env", verbose=True)
 
 class Settings:
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     DB_PATH = os.getenv("DB_PATH", "./zadavalnik.db")
+
+    
